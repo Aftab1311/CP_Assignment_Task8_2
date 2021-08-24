1e7149c0e2aa361a402cefb9470778e0e323d60f
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-24 07:16:19 +0530

    Add files via upload

---
+++ task8_2.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char A[80];
+    cout << "Enter A: ";
+    cin.getline(A, 80);
+    cout << "Duplicate characters are : " ;
+    for (int i = 0; i < strlen(A); i++)
+    {
+        for (int j = i + 1; j < strlen(A); j++)
+        {
+            if (A[i] == A[j])
+            {
+                cout << A[i]<<" " ;
+                break;
+            }
+        }
+    }
+    return 0;
+}
